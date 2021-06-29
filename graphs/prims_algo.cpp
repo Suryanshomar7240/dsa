51ea46e8d81fcaa876279b50053e6b0a63ff72cc
Suryanshomar7240 <suryu7240@gmail.com>
2021-06-29 14:54:39 +0530

    prims_algo

--- graphs/prims_algo.cpp
+++ graphs/prims_algo.cpp
@@ -8,83 +8,71 @@ typedef long long ll;
 #define error(x) cout << x << " ";
 using namespace std;
 //*******************************************************
-//           Question of Minimum spanning tree
+//                    Question
+// finding cost for traverseing from source node
 //*******************************************************
 
 //*******************************************************
 //                      MAIN
 //*******************************************************
-class dsu
-{
-public:
-    int *parent;
-    int *sze;
-    dsu(int n)
-    {
-        parent = new int[n];
-        sze=new int[n];
-        rep(i, 0, n)
-        {
-            parent[i] = i;
-            sze[i] = 1;
-        }
-    }
-    int find_parent(int a)
-    {
-        if (a == parent[a])
-            return a;
-        return parent[a] = find_parent(parent[a]);
-    }
-    void uni(int a, int b)
-    {
-        if (parent[a] == parent[b])
-            return;
-        if (sze[parent[a]] > sze[parent[b]])
-        {
-            parent[b] = parent[a];
-            sze[parent[a]] += sze[parent[b]];
-        }
-        else
-        {
-            parent[a] = parent[b];
-            sze[parent[b]] += sze[parent[a]];
-        }
-    }
-};
 int main()
 {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> adj(n + 1);
+    vector<vector<pair<int, int>>> adj(n);
     vector<vector<int>> edges;
-    const int inf=1e9;
-    vector<int> dis(n,inf);
-    vector<bool> visited(n,false);
-    int u, v,w;
+    const int inf = 1e9;
+    vector<int> dis(n, inf);
+    vector<bool> visited(n, false);
+    int u, v, w;
     for (int i = 0; i < m; i++)
     {
-        cin >> u >> v>>w;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-        edges.push_back({w,u,v});
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
     }
-    set<pair<int,int>> s;
-    s.insert({0,0});
-    visited[0]=true;
-    int cost=0;
-    dsu se(n);
-    while(!s.empty())
+    // for(auto i:adj)
+    // {
+    //     for(auto j:i)
+    //     {
+    //         cout<<j.first<<" "<<j.second<<" | ";
+    //     }
+    //     cout<<endl;
+    // }
+    int source;
+    cin >> source;
+    dis[source] = 0;
+    set<pair<int, int>> s;
+    // {wt, node}
+    s.insert({0, source});
+    // visited[source]=true;
+    int cost = 0;
+    while (!s.empty())
     {
-        auto x=*(s.begin());
-        for(auto i:ad
-        j[x.second])
+        auto x = *(s.begin());
+        s.erase(x);
+        visited[x.second] = true;
+        int u = x.second;
+        int w = x.first;
+        cout << w<< endl;
+        cost += w;
+        for (auto i : adj[u])
         {
-            if(!visited[i])
+            if (visited[i.first])
+                continue;
+            else if (dis[i.first] == inf)
             {
-                s.insert()
+                dis[i.first] = i.second;
+                s.insert({dis[i.first], i.first});
+            }
+            else if (dis[i.first] > i.second)
+            {
+                s.erase(make_pair(dis[i.first], i.first));
+                dis[i.first] = i.second;
+                s.insert(make_pair(dis[i.first], i.first));
             }
         }
     }
-    cout<<"Total cost="<<cost;
+    cout << "Total cost=" << cost;
     return 0;
 }
